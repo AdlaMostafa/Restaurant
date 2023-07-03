ea537b860dac8b5df00229066a38ed43d6e0068e
Adla Mostafa <adlamostafa422004@gmail.com>
2023-07-03 08:35:52 +0300

    Adding Home Page

---
+++ src/Pages/index.jsx
@@ -0,0 +1,12 @@
+//Home Page
+import React, { Component } from 'react'
+import Section1 from './Sections/Section1'
+export default class HomePage extends Component {
+  render() {
+    return (
+        <div id='home'>
+         <Section1/>
+        </div>
+    )
+  }
+}
