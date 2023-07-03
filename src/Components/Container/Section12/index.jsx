604f28da8d716097484f60a6f4c78be2c75bdf5b
Adla Mostafa <adlamostafa422004@gmail.com>
2023-07-03 17:55:45 +0300

    Adding Section12

---
+++ src/Components/Container/Section12/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './style.css'
+export default class Section12 extends Component {
+  render() {
+    return (
+      <div>
+        <div className='Section12'>
+            <div className='top'><p >
+            Copyright © 2022 Golden Dine website  </p></div> 
+             <div className='bot'><p >All rights reserved</p> </div>      
+        </div>
+      </div>
+    )
+  }
+}
